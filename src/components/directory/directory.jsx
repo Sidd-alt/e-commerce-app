2ec606c6ac2b53af797a63601d82b56b78e7eaf7
SIDDHANT VINCHURKAR <you@example.com>
2022-03-19 17:17:03 +0530

    updating directory page with category item component

---
+++ src/components/directory/directory.jsx
@@ -0,0 +1,17 @@
+import './directory.scss';
+import CategoryItem from '../category-item/category-item-component';
+
+const Directory = ({categories}) => {
+
+   return (
+      <>
+         <div className="directory-container">
+            {categories.map(({id, title, imageUrl}) => (
+               <CategoryItem key={id} title={title} imageUrl={imageUrl}/>
+            ))}
+         </div>
+      </>
+   );
+}
+
+export default Directory;
