dfb409aef1c2340bc11c573c5de239e50a24131d
SIDDHANT VINCHURKAR <you@example.com>
2022-04-17 20:48:41 +0530

    adding changes

--- src/components/cart-dropdown/cart-dropdown-component.jsx
+++ src/components/cart-dropdown/cart-dropdown-component.jsx
@@ -1,11 +1,19 @@
-import Button from '../button/button-component'
-import './cart-dropdown-styles.scss'
+import Button from '../button/button-component';
+import { useNavigate } from 'react-router-dom'
+import './cart-dropdown-styles.scss';
 
 const CartDropDown = () => {
+
+   const navigate = useNavigate();
+
+   const goToCheckoutPage = () => {
+      navigate("/checkout")
+   }
+
    return(
       <div className='cart-dropdown-container'>
          <div className='cart-items'/>
-         <Button>CHECKOUT</Button>
+         <Button onClick={goToCheckoutPage}>CHECKOUT</Button>
       </div>
    )
 }
---
+++ src/routes/checkout/checkout-component.jsx
@@ -0,0 +1,11 @@
+import './checkout-styles.scss';
+
+const CheckoutPage = () => {
+   return(
+      <>
+         <h1>This is checkout page</h1>
+      </>
+   )
+}
+
+export default CheckoutPage;
